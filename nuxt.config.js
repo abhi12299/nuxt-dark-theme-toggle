6e22d888be29cd1aa25e60b224a6ecf190428398
Abhishek M <abhishek.m@so.city>
2020-05-06 12:45:29 +0530

    add theme switcher, everything works.. yay

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,16 +4,26 @@ module.exports = {
   /*
   ** Headers of the page
   */
-  head: {
-    title: 'Dark theme toggler for Nuxt.js',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+  head (c) {
+    const { req } = c.context
+    let bodyClass = ''
+    if (req && 'cookies' in req) {
+      bodyClass = req.cookies.theme === 'dark' ? 'theme-dark' : 'theme-light'
+    }
+    return {
+      title: 'Dark theme toggler for Nuxt.js',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ],
+      bodyAttrs: {
+        class: bodyClass
+      }
+    }
   },
   /*
   ** Customize the progress-bar color
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,30 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-dark-toggle
-      </h1>
-      <h2 class="subtitle">
-        Dark theme toggler for nuxt js
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div @click="toggleTheme">
+      Toggle the theme
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Cookies from 'js-cookie'
 
 export default {
-  components: {
-    Logo
+  methods: {
+    toggleTheme () {
+      let newTheme
+      if (document.body.classList.contains('theme-dark')) {
+        // toggle to light theme
+        document.body.classList.replace('theme-dark', 'theme-light')
+        newTheme = 'light'
+      } else {
+        // toggle to dark theme
+        document.body.classList.replace('theme-light', 'theme-dark')
+        newTheme = 'dark'
+      }
+      // set a cookie for ssr purposes
+      Cookies.set('theme', newTheme)
+    }
   }
 }
 </script>
@@ -46,27 +37,6 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  color: var(--font-color);
 }
 </style>
